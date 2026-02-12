db24709e59e749264ece431047982790305976f5
Syithm <syithm5mail@proton.me>
2026-02-11 21:26:20 -0500

    Create queue.lua

---
+++ queue.lua
@@ -0,0 +1,38 @@
+local queue_class = {}
+
+queue_class.__index = queue_class
+
+function queue_class.new()
+    local self = setmetatable({}, queue_class)
+	self.queue = {}
+	self.front = 1
+
+    return self, self.queue
+end
+
+function queue_class:enqueue(val)
+    table.insert(self.queue, val)
+end
+
+function queue_class:peek()
+    return self.queue[self.front]
+end
+
+function queue_class:dequeue()
+    local val = self.queue[self.front]
+
+    if val ~= nil then
+        self.queue[self.front] = nil
+
+        self.front = self.front + 1
+        
+        if self.front > #self.queue then
+            table.clear(self.queue)
+            self.front = 1
+        end
+    end
+
+    return val
+end
+
+return queue_class
